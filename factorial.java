01f6efe8a17533c9f14ee1acfce5c95c0ac12121
smitdhimar <104231844+smitdhimar@users.noreply.github.com>
2023-07-05 19:37:34 +0000

    factorial done

--- factorial.java
+++ factorial.java
@@ -1,10 +1,19 @@
-public class factorial{
- public static void main(String args[])
-  {
-    Scanner ob=new Scanner(System.in);
+import java.util.Scanner;
+
+public class factorial {
+  public static void main(String args[]) {
+    Scanner ob = new Scanner(System.in);
     System.out.println("Enter the number to find it's factorial");
-    int n=ob.nextItem();
-    factorial(n);
+    int n = ob.nextInt();
+    System.out.println(fact(n));
   }
-  public static void main()
+
+  public static int fact(int n) {
+    if (n > 0) {
+      return n * fact(n - 1);
+    } else {
+      return 1;
+    }
+
+  }
 }

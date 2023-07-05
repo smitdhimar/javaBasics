14c175fe32229da6365da427a3339803fdaa1ae4
smitdhimar <104231844+smitdhimar@users.noreply.github.com>
2023-07-05 17:23:33 +0000

    Initial commit

---
+++ executable.java
@@ -0,0 +1,6 @@
+public class executable{
+  public static void main(String args[])
+  {
+    System.out.println("hello this is my first program on java");
+  }
+}
---
+++ printingStringsAndOperators.java
@@ -0,0 +1,6 @@
+public class printingStringsAndOperators {
+  public static void main(String args[]) {
+    int i = 5, j = 8;
+    System.out.println("i+j=" + i + j);
+  }
+}

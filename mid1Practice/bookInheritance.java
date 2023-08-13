83481dc4c0d3c0cfae1dbfa6eb2cc8a3fbdc470d
smitdhimar <104231844+smitdhimar@users.noreply.github.com>
2023-08-13 13:17:30 +0000

    inheritance , book program

---
+++ mid1Practice/bookInheritance.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.Scanner;
+
+class books {
+  String title;
+  String author;
+
+  books() {
+    this.title = "";
+    this.author = "";
+  }
+
+  books(String a, String b) {
+    this.title = a;
+    this.author = b;
+  }
+}
+
+class details extends books {
+  public int price;
+  public int stock;
+
+  details() {
+    super();
+  }
+
+  details(String s1, String s2, int p, int s) {
+    super(s1, s2);
+    this.price = p;
+    this.stock = s;
+  }
+
+  String getBook() {
+    return super.title;
+  }
+
+  int getPrice() {
+    return this.price;
+  }
+
+  void update(int a, int b) {
+    this.price = a;
+    this.stock = b;
+  }
+
+  void display() {
+    System.out.println(super.title + " " + super.author + " " + this.price + " " + this.stock);
+  }
+}
+
+class bookInheritance {
+  public static void main(String arg[]) {
+    details[] obarr = new details[4];
+    obarr[0] = new details("maktub", "paulo cohelo", 200, 45);
+    obarr[1] = new details("The girl in room 105", "Chetan bhagat", 154, 40);
+    obarr[2] = new details("The alchemist", "paulo cohelo", 250, 10);
+    obarr[3] = new details("ikigai", "N/A", 195, 15);
+    System.out.println("Title                  Author             price  stock");
+    for (int i = 0; i < 4; i++)
+      obarr[i].display();
+    Scanner ob = new Scanner(System.in);
+    System.out.println("Search for the book:");
+    String sbook = ob.nextLine();
+    int i;
+    for (i = 0; i < 4; i++) {
+      String str = obarr[i].getBook();
+      if (str.equals(sbook) == true) {
+        break;
+      }
+    }
+    System.out.println("found book, now update values for price and stock:");
+    int sprice = ob.nextInt();
+    int sstock = ob.nextInt();
+    obarr[i].update(sprice, sstock);
+    System.out.println("Title                  Author             price  stock");
+    for (i = 0; i < 4; i++)
+      obarr[i].display();
+
+  }
+}
--- mid1Practice/matrix.java
+++ mid1Practice/matrix.java
@@ -1,9 +1,62 @@
-class matrix{
-  public static void main(String arg[])
-  {
-    
+import java.io.*;
+import java.util.Scanner;
+
+class matrix {
+  public static void main(String arg[]) {
+    Scanner ob = new Scanner(System.in);
+    System.out.println("for mat1 row, col: ");
+    int r1 = ob.nextInt();
+    int c1 = ob.nextInt();
+    mainmatrix mat1 = new mainmatrix(r1, c1);
+    mat1.getValues();
+    System.out.println("for mat2 row, col: ");
+    int r2 = ob.nextInt();
+    int c2 = ob.nextInt();
+    mainmatrix mat2 = new mainmatrix(r2, c2);
+    mat2.getValues();
+    mainmatrix mat3 = mat1.mul(mat2);
+    mat3.display();
   }
 }
-class mainmatrix{
-  public 
+
+class mainmatrix {
+  public int mat[][];
+  public int row;
+  public int col;
+
+  mainmatrix mul(mainmatrix a) {
+    mainmatrix m = new mainmatrix(this.row, a.col);
+    for (int i = 0; i < m.row; i++) {
+      for (int j = 0; j < m.col; j++) {
+        for (int k = 0; k < this.col; k++) {
+          m.mat[i][j] += this.mat[i][k] * a.mat[k][j];
+        }
+      }
+    }
+    return m;
+  }
+
+  mainmatrix(int a, int b) {
+    this.mat = new int[a][b];
+    this.row = a;
+    this.col = b;
+  }
+
+  void getValues() {
+    Scanner ob = new Scanner(System.in);
+    for (int i = 0; i < this.row; i++) {
+      for (int j = 0; j < this.col; j++) {
+        this.mat[i][j] = ob.nextInt();
+      }
+    }
+  }
+
+  void display() {
+    for (int i = 0; i < this.row; i++) {
+      for (int j = 0; j < this.col; j++) {
+        System.out.print(this.mat[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
 }

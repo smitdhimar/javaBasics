3efe6998af16221ae61e37d467b5c977850c8471
smitdhimar <104231844+smitdhimar@users.noreply.github.com>
2023-10-03 13:48:51 +0000

    counters, grids are done

--- mid2practice/gridLayout.java
+++ mid2practice/gridLayout.java
@@ -8,6 +8,7 @@ public class gridLayout extends Applet {
   Button btn1, btn2, btn3;
 
   public void init() {
+    setBackground(Color.BLUE);
     setLayout(new GridLayout(3, 1));
     btn1 = new Button("IN line 1");
     btn2 = new Button("In line 2");
